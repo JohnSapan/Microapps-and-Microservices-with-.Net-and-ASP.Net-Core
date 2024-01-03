0dc59b5da6fe53b835131bce632912da114f1112
John <50887763+JohnRobinSapan@users.noreply.github.com>
2024-01-03 19:00:29 +0000

    Implement infrastructure resiliency with Kubernetes

--- dotnet-resiliency/Store/Program.cs
+++ dotnet-resiliency/Store/Program.cs
@@ -1,6 +1,5 @@
 using Store.Components;
 using Store.Services;
-using Microsoft.Extensions.Http.Resilience;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSingleton<ProductService>();
@@ -9,13 +8,6 @@ builder.Services.AddHttpClient<ProductService>(c =>
     var url = builder.Configuration["ProductEndpoint"] ?? throw new InvalidOperationException("ProductEndpoint is not set");
 
     c.BaseAddress = new(url);
-}).AddStandardResilienceHandler(options =>
-{
-    options.Retry.MaxRetryAttempts = 7;
-    options.TotalRequestTimeout = new HttpTimeoutStrategyOptions
-    {
-        Timeout = TimeSpan.FromMinutes(5)
-    };
 });
 
 // Add services to the container.
